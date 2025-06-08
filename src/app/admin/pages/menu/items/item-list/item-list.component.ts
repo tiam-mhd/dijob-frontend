21a3c5154b5edef13120d6228f591e21a29bf486
Mohammadi Tiam <t.mohammadi@PC-334>
2025-06-08 16:40:37 +0330

    commit-aien-14040318

--- src/app/admin/pages/menu/items/item-list/item-list.component.ts
+++ src/app/admin/pages/menu/items/item-list/item-list.component.ts
@@ -5,6 +5,7 @@ import { MenuItem } from '../../../../../shared/models/menu-item';
 import { MenuItemService } from '../../../../../shared/services/menu-item.service';
 import { MenuCategoryService } from '../../../../../shared/services/menu-category.service';
 import { MenuCategory } from '../../../../../shared/models/menu-category';
+import { GlobalService } from '../../../../../shared/services/global.service';
 
 @Component({
   selector: 'app-item-list',
@@ -14,6 +15,8 @@ import { MenuCategory } from '../../../../../shared/models/menu-category';
   styleUrl: './item-list.component.scss'
 })
 export class ItemListComponent {
+  baseUrl = "";
+
   cafeId = localStorage.getItem("cafeId");
 
   crudModalTitle = "افزودن آیتم جدید";
@@ -28,8 +31,11 @@ export class ItemListComponent {
   imageFile: File | null = null;
   imagePreview: string | ArrayBuffer | null = null;
 
-  constructor(private fb: FormBuilder, private categoryService: MenuCategoryService, private itemService: MenuItemService) {
-
+  constructor(private fb: FormBuilder,
+    private categoryService: MenuCategoryService,
+    private itemService: MenuItemService,
+    private globalService: GlobalService) {
+    this.baseUrl = globalService.apiUrl;
     this.itemForm = this.fb.group({ title: '', price: 0, categoryId: 0, description: '' } as MenuItem);
     this.loadMenuItems();
     this.loadCategories();
@@ -39,7 +45,9 @@ export class ItemListComponent {
     let id = localStorage.getItem("cafeId");
     if (id) {
       this.itemService.getByCafe(+id).subscribe(res => {
-        this.items = res.data as MenuItem[];
+        this.items = (res.data as MenuItem[]).sort((a, b) => {
+          return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+        });
         console.log(this.items);
       });
     }
@@ -55,6 +63,8 @@ export class ItemListComponent {
   }
 
   clearSelectedItem() {
+    this.imagePreview = null;
+    this.imageFile = null;
     this.selectedItem = { id: 0 } as MenuItem;
   }
 
@@ -70,14 +80,18 @@ export class ItemListComponent {
       categoryId: +this.selectedItem.categoryId
     } as MenuItem
 
-    formData.append('body', new Blob([JSON.stringify(data)], { type: 'application/json' }));
+    formData.append('title', this.selectedItem.title);
+    formData.append('price', this.selectedItem.price.toString());
+    formData.append('description', this.selectedItem.description);
+    formData.append('cafeId', (this.cafeId) ? this.cafeId.toString() : "0");
+    formData.append('categoryId', this.selectedItem.categoryId.toString());
 
     if (this.imageFile) {
       formData.append('image', this.imageFile);
     }
 
     if (this.selectedItem.id != 0) {
-      this.itemService.update(this.selectedItem.id, formData).subscribe(() => this.loadCategories());
+      this.itemService.update(this.selectedItem.id, formData).subscribe(() => this.loadMenuItems());
     } else {
       this.itemService.create(formData).subscribe(() => this.loadMenuItems());
     }
